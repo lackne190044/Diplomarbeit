9e8e0ed49ddf7277fca3dd8d1a6f8195ac308488
rettichsimonlel <retter190085@sr.htlweiz.at>
2023-10-31 13:17:39 +0100

    try connection between typescript and influxdb

---
+++ tig-stack/TYPESCRIPT.md
@@ -0,0 +1,11 @@
+## Installation
+npm init
+npm install typescript 
+npm i @influxdata/influxdb-client
+npm i --save-dev @types/node
+
+## Compile code
+npx tsc
+
+## Starting the containers again
+sudo docker-compose up --build
--- tig-stack/typescript-app/dist/app.js
+++ tig-stack/typescript-app/dist/app.js
@@ -1,3 +1,8 @@
 // app.ts
 const message = 'Hello, World!';
 console.log(message);
+const { InfluxDB } = require('@influxdata/influxdb-client'); // You can generate an API token from the "API Tokens Tab" in the UI
+const token = '6b0bd7cfadba46e46c53747166365971';
+const org = 'school';
+const bucket = 'telegraf';
+const client = new InfluxDB({ url: 'http://localhost:8086', token: token });
